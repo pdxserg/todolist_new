01ba4d4389b17dcb51a0e0c2f5e6606d1b5edd69
SergeyStarchenko <pdxserg@gmail.com>
2024-06-02 23:18:24 -0700

    create useState filter

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,36 @@ import React, {useState} from 'react';
 import {Todolist} from "./Todolist";
 
 
- export  type TaskPropseType= {
+export  type TaskPropseType = {
 	id: string
 	title: string
 	isDone: boolean
 }
- export const App = () => {
- 	const [tasks,setTasks]=useState<Array<TaskPropseType>>([
-			 {id: "1", title: "CSS", isDone: true},
-			 {id: "2", title: "JS", isDone: true},
-			 {id: "3", title: "React", isDone: false},
-		 ]
+export const App = () => {
+	const [tasks, setTasks] = useState<Array<TaskPropseType>>([
+		{id: "1", title: "CSS", isDone: true},
+		{id: "2", title: "JS", isDone: true},
+		{id: "3", title: "React", isDone: false},
+	])
+	const [filter, setFilter] = useState("ACTIVE")
+	let filteredTasks = tasks
+	if (filter === "ACTIVE") {
+		filteredTasks = filteredTasks.filter(f => f.isDone === true)
+	}
+	if (filter === "ACTIVE") {
+		filteredTasks = filteredTasks.filter(f => f.isDone === true)
+	}
 
-	 )
-
-const removeTask =(taskId:string)=>{
-const newTasks = tasks.filter(f=>f.id !== taskId)
-	setTasks(newTasks)
-}
+	const removeTask = (taskId: string) => {
+		const newTasks = tasks.filter(f => f.id !== taskId)
+		setTasks(newTasks)
+	}
 
 	return (
 		<div className="app">
 			<Todolist
 				title={"What  to learn"}
-				tasks={tasks}
+				tasks={filteredTasks}
 				removeTask={removeTask}
 			/>
 		</div>
