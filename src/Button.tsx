a362d74f01c7fd37ba6242ae5552837f03ebd13f
SergeyStarchenko <pdxserg@gmail.com>
2024-06-02 22:45:45 -0700

    create component Button add event

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {MouseEvent} from "react";
 type ButtonPropsType={
-
+onClick:(event:MouseEvent<HTMLButtonElement>)=>void
 }
- export const Button = () => {
+ export const Button = ({onClick}:ButtonPropsType) => {
+	const onClickHandler=(event:MouseEvent<HTMLButtonElement>)=>{
+		onClick(event)
+	 }
 	return (
-		<button onClick={onClick}></button>
+		<button onClick={onClickHandler}></button>
 	)
 }
