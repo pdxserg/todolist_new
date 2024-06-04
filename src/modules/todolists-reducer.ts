2aa8c15d13872eef260ffd24befa006752e70c8b
SergeyStarchenko <pdxserg@gmail.com>
2024-06-04 16:05:47 -0700

    create test

--- src/modules/todolists-reducer.ts
+++ src/modules/todolists-reducer.ts
@@ -1,25 +1,46 @@
+import {v1} from "uuid";
 
 
 type todolistsReducerTyp={
-	type:any
-	playholder:{
-
-	}
+	type:string
+	payload:Object
 }
 
 class TodolistType {
 }
+type removeTodoListType={
+	type: string
+	payload:{
+		id:string
+	}
+}
+type ActionsType =removeTodoListType
 
-export const todolistsReducer=(state:TodolistType[], action:todolistsReducerTyp):TodolistType[]=>{
+let todolistID1 = v1()
+let todolistID2 = v1()
+
+const initialState: TodolistType[] = [
+	{ id: todolistID1, title: 'What to learn', filter: 'all' },
+	{ id: todolistID2, title: 'What to buy', filter: 'all' },
+]
+export const todolistsReducer=(state:TodolistType[]=initialState, action:ActionsType):TodolistType[]=>{
 	switch (action.type){
-		case "AAAA":{
+		case "REMOVE-TODOLIT":
 			return state
-		} case "DDD":{
+		 case "DDD":
 			return state
-		} default:{
+		 default:
 			return state
-		}
-
 
 	}
+}
+
+const removeTodoListAC=(id: string):removeTodoListType=>{
+	return {
+		type:"REMOVE-TODOLIsT",
+		payload:{
+			id
+		}
+
+	} as const
 }
