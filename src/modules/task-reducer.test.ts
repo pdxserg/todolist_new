e1f91eb8d83a233ba291027478c8eeb8a3f60709
SergeyStarchenko <pdxserg@gmail.com>
2024-06-05 14:23:35 -0700

    test  "CHANGE-STATUS-TASK" reducer

--- src/modules/task-reducer.test.ts
+++ src/modules/task-reducer.test.ts
@@ -1,7 +1,7 @@
 
 import {v1} from "uuid";
 import {TasksPropsType} from "../App";
-import {removeTaskAC, tasksReducer} from "./task-reducer";
+import {changeTaskStatusAC, removeTaskAC, tasksReducer} from "./task-reducer";
 
 
 
@@ -27,5 +27,30 @@ test("remove task ", () => {
 	const endState = tasksReducer(initial, removeTaskAC(todolistID1,"1" ))
 	expect(endState[todolistID1].length).toBe(2)
 	expect(endState[todolistID1][0].title).toBe('JS')
+
+})
+
+test("CHANGE-STATUS-TASK", () => {
+	let todolistID1 = v1()
+	let todolistID2 = v1()
+
+
+	let initial:TasksPropsType = ({
+		[todolistID1]: [
+			{id: "1", title: 'HTML&CSS', isDone: true},
+			{id: "2", title: 'JS', isDone: true},
+			{id: "3", title: 'ReactJS', isDone: false},
+		],
+		[todolistID2]: [
+			{id: v1(), title: 'Rest API', isDone: true},
+			{id: v1(), title: 'GraphQL', isDone: false},
+		],
+	})
+
+	const endState = tasksReducer(initial, changeTaskStatusAC(todolistID1,"2", false))
+
+	expect(endState[todolistID1].length).toBe(3)
+	expect(endState[todolistID1][1].isDone).toBe(false)
+	expect(endState[todolistID1][2].isDone).toBe(false)
 
 })
