775fdde5361de85d6f21214d703b289b14911327
SergeyStarchenko <pdxserg@gmail.com>
2024-06-08 23:42:29 -0700

    todolists-tasks-reducer.test.ts-tasks-test

---
+++ src/components/todolists-tasks-reducer.test.ts
@@ -0,0 +1,22 @@
+import {TasksPropsType, TodolistType} from "../App";
+import {tasksReducer} from "../modules/task-reducer";
+import {addTodoListAC, todolistsReducer} from "../modules/todolists-reducer";
+
+test('ids should be equals', () => {
+	const startTasksState: TasksPropsType = {}
+	const startTodolistsState: Array<TodolistType> = []
+
+	const action = addTodoListAC('new todolist')
+
+	const endTasksState = tasksReducer(startTasksState, action)
+	const endTodolistsState = todolistsReducer(startTodolistsState, action)
+
+	const keys = Object.keys(endTasksState)
+	const idFromTasks = keys[0]
+	const idFromTodolists = endTodolistsState[0].id
+
+	expect(idFromTasks).toBe(action.id)
+	expect(idFromTodolists).toBe(action.id)
+})
+
+
